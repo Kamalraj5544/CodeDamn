0148cac34a3ccce77b2691432ee90778885d6534
Kamal raj k <kamalraj5544@gmail.com>
2023-03-19 21:46:46 +0530

    completed day 2 challenge

--- 50DaysOfJS.js
+++ 50DaysOfJS.js
@@ -6,4 +6,28 @@ function randomNumberGeneratorInRange(rangeStart, rangeEnd) {
   return (Math.floor(Math.random() * rangeStart + 1) + rangeEnd) - rangeStart;
 }
 
-console.log(`My random number: ${randomNumberGeneratorInRange(5, 20)}`);
+// console.log(`My random number: ${randomNumberGeneratorInRange(5, 20)}`);
+
+/*
+Question : Write a program to reverse a string
+              1.String can be reversed by iterating it and storing it in reverse order
+              2. String can also be reversed by converting it to array, then joining it after reversing
+*/
+
+
+const str = "JavaScript is awesome"
+
+function reverseAString(str) {
+    // write your solution here
+    let strArr = str.split("")
+    
+    for (let i = 0; i < strArr.length/2; i++) {
+      const temp = strArr[i];
+      strArr[i] = strArr[strArr.length-1-i];
+      strArr[strArr.length-1-i]= temp;
+    }
+
+    return strArr.join("");
+}
+
+console.log(`Reversed string is: ${reverseAString(str)}`)

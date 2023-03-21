d6af98bc844e5899b3b3d043ef0cedfbb152065b
Kamal raj k <kamalraj5544@gmail.com>
2023-03-21 18:14:24 +0530

    day 4 completed

--- 50DaysOfJS.js
+++ 50DaysOfJS.js
@@ -8,10 +8,14 @@ function randomNumberGeneratorInRange(rangeStart, rangeEnd) {
 
 // console.log(`My random number: ${randomNumberGeneratorInRange(5, 20)}`);
 
+
+
+// Day 2 challange
+
 /*
 Question : Write a program to reverse a string
-              1.String can be reversed by iterating it and storing it in reverse order
-              2. String can also be reversed by converting it to array, then joining it after reversing
+1.String can be reversed by iterating it and storing it in reverse order
+2. String can also be reversed by converting it to array, then joining it after reversing
 */
 
 function reverseAString(str) {
@@ -23,15 +27,17 @@ function reverseAString(str) {
       strArr[i] = strArr[strArr.length-1-i];
       strArr[strArr.length-1-i]= temp;
     }
-
+    
     return strArr.join("");
-}
-
-// console.log(`Reversed string is: ${reverseAString("JavaScript is awesome")}`)
-
-
-
-function reverseGivenInteger(num) {
+  }
+  
+  // console.log(`Reversed string is: ${reverseAString("JavaScript is awesome")}`)
+  
+  
+  // Day 3 challange
+  
+  
+  function reverseGivenInteger(num) {
     // write your solution here
     let ans = 0;
     while(num > 0) {
@@ -42,5 +48,33 @@ function reverseGivenInteger(num) {
     return ans
 }
 
-console.log(`Reversed integer is: ${reverseGivenInteger(3849)}`)
+// console.log(`Reversed integer is: ${reverseGivenInteger(3849)}`)
+
+
+
+// Day 3 challange
+
+// Write a function which can convert the time input given in 12 hours format to 24 hours format
+
+function convertTo24HrsFormat(time) {
+  // write your solution here
+
+  if(time.endsWith("AM") && time.slice(0,2) === "12"){
+    return "00"+time.slice(2,5);
+  }else if (time.endsWith("AM") && time.length === 6){
+    return "0"+time.slice(0,4);
+  }else if (time.endsWith("AM")){
+    return time.slice(0,5);
+  }else if(time.endsWith("PM")  && time.slice(0,2) === "12"){
+    return time.slice(0,5);
+  }
+
+    let railwayTime = (+time.slice(0,2))+12;
+    return railwayTime+time.slice(2,5);
+
+}
+
+console.log(`Converted time: ${convertTo24HrsFormat("5:00AM")}`)
+
+
 

0183c1b66d48961f0e45f9135e04e9ce414e8228
Kamal raj k <kamalraj5544@gmail.com>
2023-03-28 21:59:40 +0530

    day 10 complted

--- 50DaysOfJS.js
+++ 50DaysOfJS.js
@@ -129,15 +129,15 @@ const DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24;
 
 function getDaysBetweenDates(dateText1, dateText2) {
   // write your solution here
-  let date1 = new Date(dateText1).getTime(), date2 = new Date(dateText2).getTime();
-  let requiredDays = (date2-date1) /DAY_IN_MILLISECONDS
-  return requiredDays
+  let date1 = new Date(dateText1).getTime(),
+    date2 = new Date(dateText2).getTime();
+  let requiredDays = (date2 - date1) / DAY_IN_MILLISECONDS;
+  return requiredDays;
 }
 
 // console.log(
 //   `Days difference: ${getDaysBetweenDates("10/15/2020", "12/1/2020")}`
 // );
-
 
 // Day 9 challange :
 
@@ -146,9 +146,29 @@ function getDaysBetweenDates(dateText1, dateText2) {
 const obj = { key: 1 };
 
 function isEmpty(obj) {
-    // write your solution here
+  // write your solution here
 
-    return Object.keys(obj).length === 0;
+  return Object.keys(obj).length === 0;
 }
 
-console.log(`is empty object: ${isEmpty(obj)}`)
+// console.log(`is empty object: ${isEmpty(obj)}`)
+
+// Day 10 challange :
+
+// Write a function to remove array element based on object property?
+
+const arr = [
+  { field: "id", operator: "eq" },
+  { field: "cStatus", operator: "eq" },
+  { field: "money", operator: "eq" },
+];
+
+const filterField = "money";
+
+function removeArrayElement(filterField) {
+  // write your solution here
+  const filteredArray = arr.filter((ele) => ele.field !== filterField);
+  return filteredArray;
+}
+
+console.log(`filtered array: ${removeArrayElement("id")}`);

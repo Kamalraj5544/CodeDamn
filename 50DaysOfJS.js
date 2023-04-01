ff4110bb34a627287844487c8aaf9b383327e949
Kamal raj k <kamalraj5544@gmail.com>
2023-04-01 10:52:18 +0530

    day 14 complted

--- 50DaysOfJS.js
+++ 50DaysOfJS.js
@@ -315,3 +315,28 @@ const spaceAge = (seconds) => {
 }
 
 console.log(spaceAge(Math.round(Math.random() * 99999999)))
+
+
+
+// Day 14 challange :
+
+// Convert given array of digits of a base to another asked base
+
+
+const convertToBase = (num, base, numLength = 1 + ~~(Math.log(num) / Math.log(base))) =>
+    numLength ? [...convertToBase(num / base | 0, base, numLength - 1), num % base] : [];
+
+const convertDigitsToAskedBase = (digits, baseA, baseB) => {
+	if (baseA <= 1 || baseA % 1) throw new Error('Wrong input base')
+	if (baseB <= 1 || baseB % 1) throw new Error('Wrong output base')
+	if ([!digits.length, digits.length > 1 && digits[0] === 0, digits.some((digit) => digit < 0 || digit >= baseA)].some((assertion) => assertion))
+		throw new Error('Input has wrong format')
+	// Convert digits in base a to base 10 then convert that number to base b.
+	return convertToBase(
+		digits.reduce((num, digit) => digit + num * baseA),
+		baseB
+	)
+}
+
+
+console.log(convertDigitsToAskedBase([5,8],10,16))

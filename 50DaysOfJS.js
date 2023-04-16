c83286620d2b7f2e6f6b077328baecaf27147f9a
Kamal raj k <kamalraj5544@gmail.com>
2023-04-16 09:57:04 +0530

    day 28 completed

--- 50DaysOfJS.js
+++ 50DaysOfJS.js
@@ -709,4 +709,29 @@ function getCount(str) {
   return vowelsCount;
 }
 
-console.log(getCount("abracadabra"));
+// console.log(getCount("abracadabra"));
+
+
+// Day 28 challange
+
+// WeIrD StRiNg CaSe
+
+// Write a function toWeirdCase that accepts a string, and returns 
+// the same string with all even indexed characters in each word upper cased, 
+// and all odd indexed characters in each word lower cased. 
+// The indexing just explained is zero based, so the zero-ith index is even,
+//  therefore that character should be upper cased and you need to start over for each word.
+
+
+function toWeirdCase(string) {
+  // Your code goes here
+  let strArr = [...string].map((c,i) => {
+    if( i % 2 == 0) return c.toUpperCase();
+    else return c.toLowerCase();
+  })
+  return strArr.join("");
+ }
+
+console.log(
+  `The weird case of ${"A test case"} is ${toWeirdCase("A test case")}`
+);
